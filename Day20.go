70d78b0e051f64e6eeb237cf31c7b206eb8eef83
Nafisur <nafisur@Nafisurs-MacBook-Air.local>
2020-05-20 21:28:30 +0530

    First commit

---
+++ Day20.go
@@ -0,0 +1,45 @@
+package leetcode
+
+/*
+Kth Smallest Element in a BST
+Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+
+Note:
+You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+
+Example 1:
+Input: root = [3,1,4,null,2], k = 1
+   3
+  / \
+ 1   4
+  \
+   2
+Output: 1
+
+*/
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func kthSmallest(root *TreeNode, k int) int {
+	stack := []TreeNode{}
+
+	for {
+		for root != nil {
+			stack = append(stack, *root)
+			root = root.Left
+		}
+
+		root = &stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		k--
+		if k == 0 {
+			return root.Val
+		}
+		root = root.Right
+	}
+}
