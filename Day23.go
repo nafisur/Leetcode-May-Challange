ae39a535dd8182f7ef451948d4843f422a7d2a98
Nafisur <nafisur@Nafisurs-MacBook-Air.local>
2020-05-23 23:42:44 +0530

    First commit

---
+++ Day23.go
@@ -0,0 +1,45 @@
+package leetcode
+
+/*
+Interval List Intersections
+Solution
+Given two lists of closed intervals, each list of intervals is pairwise disjoint and in sorted order.
+
+Return the intersection of these two interval lists.
+
+(Formally, a closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.
+The intersection of two closed intervals is a set of real numbers that is either empty,
+or can be represented as a closed interval.
+For example, the intersection of [1, 3] and [2, 4] is [2, 3].)
+
+Input: A = [[0,2],[5,10],[13,23],[24,25]], B = [[1,5],[8,12],[15,24],[25,26]]
+Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+Reminder: The inputs and the desired output are lists of Interval objects, and not arrays or lists.
+*/
+
+func intervalIntersection(A [][]int, B [][]int) [][]int {
+	result := [][]int{}
+	for i, j := 0, 0; i < len(A) && j < len(B); {
+		_, left := minMax(A[i][0], B[j][0])
+		right, _ := minMax(A[i][1], B[j][1])
+		if left <= right {
+			result = append(result, []int{left, right})
+		}
+		if A[i][1] < B[j][1] {
+			i++
+		} else if A[i][1] > B[j][1] {
+			j++
+		} else {
+			i++
+			j++
+		}
+	}
+	return result
+}
+
+func minMax(a, b int) (int, int) {
+	if a < b {
+		return a, b
+	}
+	return b, a
+}
